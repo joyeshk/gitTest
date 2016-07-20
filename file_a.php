645c108b09bf52fa6953b459b17cdcec6c28c272
Joyesh Karmakar <joyesh@codomotive.com>
2016-07-20 19:24:22 +0530

    Update file_a.php
    added new non conflict line

--- file_a.php
+++ file_a.php
@@ -3,6 +3,7 @@
 
 /*edit this line multiple times from different account edit from master user added line from local edit1*/
 
+/*added a new line from web*/
 
 /*static lines add in between new lines from different account*/
 /*static lines*/
