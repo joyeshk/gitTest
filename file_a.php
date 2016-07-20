53797eb26f84f1f6d164e11d0ff7b92e5103f1b1
Joyesh Karmakar <joyesh@codomotive.com>
2016-07-20 17:17:29 +0530

    Create file_a.php
    this is a initial file for understanding git.

---
+++ file_a.php
@@ -0,0 +1,12 @@
+/*this is a new file created by master user*/
+
+/*edit this line multiple times from different account*/
+
+/*static lines add in between new lines from different account*/
+/*static lines*/
+/*static lines*/
+/*static lines*/
+/*static lines*/
+/*static lines*/
+/*static lines*/
+/*static lines*/
