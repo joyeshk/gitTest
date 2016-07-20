316493511fb8f9c7105d10e23848bc153b1befb0
Joyesh Karmakar <joyesh@codomotive.com>
2016-07-20 18:00:23 +0530

    change from local
    change from local

--- file_a.php
+++ file_a.php
@@ -1,6 +1,6 @@
 /*this is a new file created by master user*/
 
-/*edit this line multiple times from different account edit from master user*/
+/*edit this line multiple times from different account edit from master user added line from local*/
 
 /*static lines add in between new lines from different account*/
 /*static lines*/
